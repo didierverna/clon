306c46d4ecc4632920023fad7c7796f6e99098ee
didier <didier@lrde.epita.fr>
2008-12-11 21:11:00 +0100

    Added usage.lisp

---
+++ src/output/usage.lisp
@@ -0,0 +1,46 @@
+;;; usage.lisp --- The Usage Protocol
+
+;; Copyright (C) 2008 Didier Verna
+
+;; Author:        Didier Verna <didier@lrde.epita.fr>
+;; Maintainer:    Didier Verna <didier@lrde.epita.fr>
+;; Created:       Tue Dec  2 11:01:06 2008
+;; Last Revision: Tue Dec  2 11:01:06 2008
+
+;; This file is part of Clon.
+
+;; Clon is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+
+;; Clon is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+;;; Commentary:
+
+;; Contents management by FCM version 0.1.
+
+
+;;; Code:
+
+(in-package :clon)
+(in-readtable :clon)
+
+
+(defgeneric %usage (sheet item)
+  (:documentation "Describe usage of ITEM on SHEET.")
+  (:method (sheet item)
+    (values))
+  (:method (sheet (synopsis synopsis))
+    (print synopsis)))
+
+
+;;; usage.lisp ends here
