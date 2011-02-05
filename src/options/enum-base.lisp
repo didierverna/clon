ffc3f9cbfca16943dccd1fb251fc1a742b71efc9
Didier Verna <didier@lrde.epita.fr>
2011-02-05 20:53:39 +0100

    Switch to a BSD-style license.

--- src/options/enum-base.lisp
+++ src/options/enum-base.lisp
@@ -1,26 +1,37 @@
 ;;; enum-base.lisp --- Base for enumeration-like options
 
-;; Copyright (C) 2010 Didier Verna
+;; Copyright (C) 2010, 2011 Didier Verna
 
 ;; Author:        Didier Verna <didier@lrde.epita.fr>
 ;; Maintainer:    Didier Verna <didier@lrde.epita.fr>
-;; Created:       Wed Aug 11 21:34:31 2010
-;; Last Revision: Wed Aug 11 21:35:14 2010
 
 ;; This file is part of Clon.
 
-;; Clon is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License version 3,
-;; as published by the Free Software Foundation.
+;; Redistribution and use in source or binary form, with or without
+;; modification, are permitted provided that the following conditions are met:
 
-;; Clon is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Redistributions of source code must retain the above copyright notice, this
+;; list of conditions and the following disclaimer.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;; Redistributions in binary form must reproduce the above copyright notice,
+;; this list of conditions and the following disclaimer in the documentation
+;; and/or other materials provided with the distribution.
+
+;; Neither the names of the authors or copyright holders, nor the names of any
+;; contributor or organization may be used to endorse or promote products
+;; derived from Clon without specific prior written permission.
+
+;; CLON IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+;; EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;; POSSIBILITY OF SUCH DAMAGE.
 
 
 ;;; Commentary:
