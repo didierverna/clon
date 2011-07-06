a70aec3f67b2dd651ffb6f7bb3627cd9551dcd54
Didier Verna <didier@lrde.epita.fr>
2011-07-06 17:26:49 +0200

    Coding style update.
    src/ ChangeLog addition:
    
    2011-07-06  Didier Verna  <didier@lrde.epita.fr>
    
    	* util.lisp (maybe-push):
    	* util.lisp (accumulate):
    	* util.lisp (beginning-of-string-p):
    	* util.lisp (closest-match): Provide emacs indentation infor via
    	swank, update indentation and use of &aux.

--- src/util.lisp
+++ src/util.lisp
@@ -43,61 +43,66 @@
 ;; Miscellaneous Auxiliary Routines
 ;; ==========================================================================
 
+#+swank (swank:eval-in-emacs
+	 '(put 'econd 'common-lisp-indent-function
+	   (get 'cond 'common-lisp-indent-function))
+	 t)
 (defmacro econd (&body clauses)
   "Like COND, but signal an error if no clause evaluates to t."
   `(cond ,@(append clauses
-		   '((t (error "Fell out of ECOND clauses."))))))
+	    '((t (error "Fell out of ECOND clauses."))))))
 
 (defmacro endpush (object place)
   "Like push, but at the end."
   `(setf ,place (nconc ,place (list ,object))))
 
-(defmacro maybe-push (object place)
+(defmacro maybe-push (object place &aux (the-object (gensym "object")))
   "Like push, but only if OBJECT is non-nil."
-  (let ((the-object (gensym "object")))
-    `(let ((,the-object ,object))
-      (when ,the-object (push ,the-object ,place)))))
+  `(let ((,the-object ,object))
+     (when ,the-object (push ,the-object ,place))))
 
-(defmacro accumulate ((initial-value) &body body)
+(defmacro accumulate
+    ((initial-value) &body body
+		     &aux (place (gensym "place"))
+			  (initial-place (gensym "initial-place")))
   "Accumulate BODY forms in a list beginning with INITIAL-VALUE.
 INITIAL-VALUE is not evaluated. BODY forms are accumulated only when their
 value is non-nil.
 If nothing to accumulate, then return nil instead of the list of
 INITIAL-VALUE."
-  (let ((place (gensym "place"))
-	(initial-place (gensym "initial-place")))
-    `(let* ((,place (list ',initial-value))
-	    (,initial-place ,place))
-      ,@(mapcar (lambda (body-form)
-		  `(maybe-push ,body-form ,place))
-		body)
-      (when (not (eq ,initial-place ,place))
-	(nreverse ,place)))))
+  `(let* ((,place (list ',initial-value))
+	  (,initial-place ,place))
+     ,@(mapcar (lambda (body-form)
+		 `(maybe-push ,body-form ,place))
+	       body)
+     (when (not (eq ,initial-place ,place))
+       (nreverse ,place))))
 
-(defun beginning-of-string-p (beginning string &optional ignore-case)
+(defun beginning-of-string-p
+    (beginning string &optional ignore-case &aux (length (length beginning)))
   "Check that STRING starts with BEGINNING.
 If IGNORE-CASE, well, ignore case."
-  (let ((length (length beginning)))
-    (and (>= (length string) length)
-	 (funcall (if ignore-case #'string-equal #'string=)
-		  beginning string :end2 length))))
+  (and (>= (length string) length)
+       (funcall (if ignore-case #'string-equal #'string=)
+		beginning string :end2 length)))
 
-(defun closest-match (match list &key ignore-case (key #'identity))
+(defun closest-match
+    (match list &key ignore-case (key #'identity)
+		&aux (match-length (length match))
+		     (shortest-distance most-positive-fixnum)
+		     closest-match)
   "Return the LIST element closest to MATCH, or nil.
 If IGNORE-CASE, well, ignore case.
 KEY should provide a way to get a string from each LIST element."
-  (let ((match-length (length match))
-	(shortest-distance most-positive-fixnum)
-	closest-match)
-    (dolist (elt list)
-      (let ((elt-string (funcall key elt))
-	    distance)
-	(when (and (beginning-of-string-p match elt-string ignore-case)
-		   (< (setq distance (- (length elt-string) match-length))
-		      shortest-distance))
-	  (setq shortest-distance distance)
-	  (setq closest-match elt))))
-    closest-match))
+  (dolist (elt list)
+    (let ((elt-string (funcall key elt))
+	  distance)
+      (when (and (beginning-of-string-p match elt-string ignore-case)
+		 (< (setq distance (- (length elt-string) match-length))
+		    shortest-distance))
+	(setq shortest-distance distance)
+	(setq closest-match elt))))
+  closest-match)
 
 (defun complete-string (beginning complete)
   "Complete BEGINNING with the rest of COMPLETE in parentheses.
@@ -111,8 +116,8 @@ For instance, completing 'he' with 'help' will produce 'he(lp)'."
 - KEY should provide a way to get a string from each LIST element.
 - SEPARATOR is the string to insert between elements."
   (reduce (lambda (str1 str2) (concatenate 'string str1 separator str2))
-	  list
-	  :key key))
+      list
+    :key key))
 
 
 
@@ -125,25 +130,28 @@ For instance, completing 'he' with 'help' will produce 'he(lp)'."
   (loop :for key :in keys :by #'cddr
 	:for val :in (cdr keys) :by #'cddr
 	:when (member key selected)
-	:nconc (list key val)))
+	  :nconc (list key val)))
 
 (defun remove-keys (keys &rest removed)
   "Return a new property list from KEYS without REMOVED ones."
   (loop :for key :in keys :by #'cddr
 	:for val :in (cdr keys) :by #'cddr
 	:unless (member key removed)
-	:nconc (list key val)))
+	  :nconc (list key val)))
 
+#+swank (swank:eval-in-emacs
+	 '(put 'replace-in-keys 'common-lisp-indent-function 3)
+	 t)
 (defmacro replace-in-keys ((key val) keys the-key form)
   "Replace every occurrence of THE-KEY in KEYS with FORM.
 At every KEYS round, KEY and VAL are bound to the current key-value pair.
 FORM is evaluated each time and should return a key-value list."
   `(loop :for ,key :in ,keys :by #'cddr
-    :for ,val :in (cdr ,keys) :by #'cddr
-    :if (eql ,key ,the-key)
-    :append ,form
-    :else
-    :nconc (list ,key ,val)))
+	 :for ,val :in (cdr ,keys) :by #'cddr
+	 :if (eql ,key ,the-key)
+	   :append ,form
+	 :else
+	   :nconc (list ,key ,val)))
 
 ;; #### NOTE: that's the typical situation where I would like a
 ;; destructuring-cond, but it seems difficult to do so because of the
@@ -184,11 +192,11 @@ REPLACEMENT can take the following forms:
 	    (replace-in-keys (key val) keys old-key
 	      (list new-key
 		    (let ((match
-			   (assoc val replacements
-				  :test (lambda (val val-or-vals)
-					  (if (consp val-or-vals)
-					      (member val val-or-vals)
-					      (eql val val-or-vals))))))
+			      (assoc val replacements
+				     :test (lambda (val val-or-vals)
+					     (if (consp val-or-vals)
+						 (member val val-or-vals)
+					       (eql val val-or-vals))))))
 		      (if match (cadr match) val))))))
 	 ((and (consp replacement)
 	       (> (length replacement) 1)
@@ -199,18 +207,19 @@ REPLACEMENT can take the following forms:
 				  :test (lambda (val val-or-vals)
 					  (if (consp val-or-vals)
 					      (member val val-or-vals)
-					      (eql val val-or-vals))))))
+					    (eql val val-or-vals))))))
 		(if match
 		    (cdr match)
-		    (list key val))))))))
+		  (list key val))))))))
 
-(defun replace-keys (keys &rest replacements)
+#+swank (swank:eval-in-emacs
+	 '(put 'replace-keys 'common-lisp-indent-function 1))
+(defun replace-keys (keys &rest replacements &aux (new-keys keys))
   "Return a new property list from KEYS with REPLACEMENTS.
 See REPLACE-KEY for more information on the replacement syntax."
-  (let ((new-keys keys))
-    (dolist (replacement replacements)
-      (setq new-keys (replace-key replacement new-keys)))
-    new-keys))
+  (dolist (replacement replacements)
+    (setq new-keys (replace-key replacement new-keys)))
+  new-keys)
 
 
 
@@ -247,7 +256,7 @@ This is the meta-class for abstract classes."))
   (when (assoc :metaclass options)
     (error "Defining abstract class ~S: explicit meta-class option." class))
   `(defclass ,class ,super-classes ,slots ,@options
-    (:metaclass abstract-class)))
+	     (:metaclass abstract-class)))
 
 (defmethod make-instance ((class abstract-class) &rest initargs)
   (declare (ignore initargs))
@@ -272,8 +281,8 @@ Both instances share the same slot values."
 	   (new-instance (make-instance (or subclass class))))
       (loop :for slot :in slots
 	    :when (slot-boundp instance (slot-definition-name slot))
-	    :do (setf (slot-value new-instance (slot-definition-name slot))
-		      (slot-value instance (slot-definition-name slot))))
+	      :do (setf (slot-value new-instance (slot-definition-name slot))
+			(slot-value instance (slot-definition-name slot))))
       new-instance)))
 
 
@@ -307,14 +316,14 @@ Both instances share the same slot values."
     (stream-file-stream (symbol-value (synonym-stream-symbol stream))))
   (:method ((stream two-way-stream) &optional direction)
     (stream-file-stream
-	(case direction
-	  (:input (two-way-stream-input-stream stream))
-	  (:output (two-way-stream-output-stream stream))
-	  (otherwise
-	   (error "Cannot extract file-stream from TWO-WAY-STREAM ~A:
+     (case direction
+       (:input (two-way-stream-input-stream stream))
+       (:output (two-way-stream-output-stream stream))
+       (otherwise
+	(error "Cannot extract file-stream from TWO-WAY-STREAM ~A:
 invalid direction: ~S"
-		  stream direction)))
-	direction)))
+	       stream direction)))
+     direction)))
 
 #+ecl
 (defun fd-line-width (fd)
@@ -375,10 +384,10 @@ Return two values:
   #+ccl
   (ccl:rlet ((winsize :winsize))
     (let ((result
-	   (ccl::int-errno-call
-	    (#_ioctl (ccl::stream-device stream :output)
-		     #$TIOCGWINSZ
-		     :address winsize))))
+	    (ccl::int-errno-call
+	     (#_ioctl (ccl::stream-device stream :output)
+		      #$TIOCGWINSZ
+		      :address winsize))))
       (if (zerop result)
 	  (ccl:pref winsize :winsize.ws_col)
 	(unless (= result (- #$ENOTTY))
@@ -504,33 +513,33 @@ this function behaves differently in some cases, as described below.
   ;; #### PORTME.
   #+ecl (declare (ignore name))
   #+sbcl  `(sb-ext:save-lisp-and-die ,name
-	    :toplevel #',function
-	    :executable t
-	    :save-runtime-options t)
+	     :toplevel #',function
+	     :executable t
+	     :save-runtime-options t)
   #+cmu   `(ext:save-lisp ,name
-	    :init-function #',function
-	    :executable t
-	    :load-init-file nil
-	    :site-init nil
-	    :print-herald nil
-	    :process-command-line nil)
+	     :init-function #',function
+	     :executable t
+	     :load-init-file nil
+	     :site-init nil
+	     :print-herald nil
+	     :process-command-line nil)
   #+ccl   `(ccl:save-application ,name
-	    :toplevel-function #',function
-	    :init-file nil
-	    :error-handler :quit
-	    :prepend-kernel t)
+	     :toplevel-function #',function
+	     :init-file nil
+	     :error-handler :quit
+	     :prepend-kernel t)
   ;; #### NOTE: ECL works differently: it needs an entry point (i.e. actual
   ;; code to execute) instead of a main function. So we expand DUMP to just
   ;; call that function.
   #+ecl   (list function)
   ;; CLISP's saveinitmem function doesn't quit, so we need to do so here.
   #+clisp `(progn
-	    (ext:saveinitmem ,name
-	     :init-function #',function
-	     :executable 0
-	     :quiet t
-	     :norc t)
-	    (exit))
+	     (ext:saveinitmem ,name
+	       :init-function #',function
+	       :executable 0
+	       :quiet t
+	       :norc t)
+	     (exit))
   #+abcl (if (boundp 'cl-user::com.dvlsoft.clon.dump)
 	     (let ((source-pathname (or *compile-file-pathname*
 					*load-pathname*))
