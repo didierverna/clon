3219c3aa94389b1db264cef24d866b3cf0476aa7
Didier Verna <didier@lrde.epita.fr>
2012-05-21 17:08:21 +0200

    Improve robustness of terminal autodetection. The code should not error anymore on unknown kinds of streams.
    ChangeLog entries:
    
    2012-05-21  Didier Verna  <didier@lrde.epita.fr>
    
    	* termio/termio.lisp (stream-file-stream):
    	* termio/termio.lisp (stream-terminal-stream): Removed.
    	* termio/termio.lisp (stream-ioctl-output-handle): New function.
    	Do the job of canonicalizing streams and retrieving handles when
    	possible at the same time.
    	* termio/termio.lisp (stream-line-width): Update accordingly.

--- termio/termio.lisp
+++ termio/termio.lisp
@@ -24,6 +24,44 @@
 
 ;; Contents management by FCM version 0.1.
 
+;; Basically, this whole stream manipulation stuff is a big non-standard mess.
+;; Terminal streams are sometimes implemented on top of file streams,
+;; sometimes not. Functions that retreive file descriptors sometimes just
+;; return nil when it's not possible, or something else, or throw errors. It
+;; is not always possible to know whether a stream has file descriptors or
+;; not. Finally, there are cases where streams have an implicit hard-wired
+;; file descriptor of 1, without any function to get it...
+
+;; Here's the information that I've gathered so far. This information is used
+;; in the implementation of STREAM-IOCTL-OUTPUT-HANDLE below.
+
+;;    SBCL:  SB-SYS:FD-STREAM for both terminals and files.
+;;    CMUCL: SYSTEM:FD-STREAM for both terminals and files.
+;;    CCL:   CCL::BASIC-CHARACTER-[INPUT|OUTPUT]-STREAM for terminals,
+;;           CCL::BASIC-FILE-CHARACTER-[INPUT|OUTPUT]-STREAM for files, with
+;;           the above somewhere in the superclasses. I'm not sure if any
+;;           class guarantees the existence of file descriptors, but it
+;;           doesn't matter because CCL:STREAM-DEVICE returns nil if there are
+;;           none, so we can safely use it on all classes.
+;;    ECL:   FILE-STREAM for both terminals and files.
+;;    CLISP: STREAM for terminals, FILE-STREAM for files. It is impossible (at
+;;           the Lisp level) to detect whether a stream has file descriptors
+;;           or not. The only workaround available right now is to catch
+;;           SYSTEM::SIMPLE-STREAM-ERROR potentially thrown by
+;;           EXT:STREAM-HANDLES.
+;;    ACL:   EXCL:TERMINAL-SIMPLE-STREAM for terminals,
+;;           EXCL:FILE-SIMPLE-STREAM for files. I'm not sure if any class
+;;           guarantees the existence of file descriptors, but it doesn't
+;;           matter because EXCL::STREAM-[INPUT|OUTPUT]-HANDLE returns
+;;           something which is not a number when there are none. The return
+;;           value seems to be either NIL or some modified version of the
+;;           original stream...
+;;    LW:    SYSTEM::TERMINAL-STREAM for terminals, STREAM::[ENC]-FILE-STREAM
+;;           for files. Terminal streams always have file descriptors 0
+;;           (input) and 1 (output) hard wired. File streams are subclasses of
+;;           STREAM::OS-FILE-HANDLE-STREAM on which we can retrieve file
+;;           handles.
+
 
 ;;; Code:
 
@@ -38,51 +76,47 @@
 #include <sys/ioctl.h>")
 
 
-;; Thanks Nikodemus!
-(defgeneric stream-file-stream (stream &optional direction)
-  (:documentation "Return STREAM's file-stream, or nil.")
-  (:method ((stream file-stream) &optional direction)
-    (declare (ignore direction))
+(defgeneric stream-ioctl-output-handle (stream)
+  (:documentation "Return STREAM's ioctl output handle or NIL.")
+  ;; Standard canonicalization methods:
+  (:method ((stream synonym-stream))
+    (stream-ioctl-output-handle (symbol-value (synonym-stream-symbol stream))))
+  (:method ((stream two-way-stream))
+    (stream-ioctl-output-handle (two-way-stream-output-stream stream)))
+  ;; #### PORTME.
+  ;; Compiler-specific implementations:
+  #+sbcl
+  (:method ((stream sb-sys:fd-stream))
+    ;; sb-posix's IOCTL function takes a stream directly
     stream)
-  (:method ((stream synonym-stream) &optional direction)
-    (declare (ignore direction))
-    (stream-file-stream (symbol-value (synonym-stream-symbol stream))))
-  (:method ((stream two-way-stream) &optional direction)
-    (stream-file-stream
-     (case direction
-       (:input (two-way-stream-input-stream stream))
-       (:output (two-way-stream-output-stream stream))
-       (otherwise
-	(error "Cannot extract file-stream from TWO-WAY-STREAM ~A:
-invalid direction: ~S"
-	       stream direction)))
-     direction))
-  (:method (stream &optional direction)
-    (declare (ignore direction))
-    #+(or ccl ecl clisp allegro) (declare (ignore stream))
-    nil))
-
-#+lispworks
-(defgeneric stream-terminal-stream (stream &optional direction)
-  (:documentation "Return STREAM's terminal-stream, or nil.")
-  (:method ((stream system::terminal-stream) &optional direction)
-    (declare (ignore direction))
-    stream)
-  (:method ((stream synonym-stream) &optional direction)
-    (declare (ignore direction))
-    (stream-terminal-stream (symbol-value (synonym-stream-symbol stream))))
-  (:method ((stream two-way-stream) &optional direction)
-    (stream-terminal-stream
-     (case direction
-       (:input (two-way-stream-input-stream stream))
-       (:output (two-way-stream-output-stream stream))
-       (otherwise
-	(error "Cannot extract terminal-stream from TWO-WAY-STREAM ~A:
-invalid direction: ~S"
-	       stream direction)))
-     direction))
-  (:method (stream &optional direction)
-    (declare (ignore direction))
+  #+cmu
+  (:method ((stream system:fd-stream))
+    (system:fd-stream-fd stream))
+  #+ecl
+  (:method ((stream file-stream))
+    (ext:file-stream-fd stream))
+  #+lispworks
+  (:method ((stream system::terminal-stream))
+    1)
+  #+lispworks
+  (:method ((stream stream::os-file-handle-stream))
+    (stream::os-file-handle-stream-file-handle stream))
+  (:method (stream)
+    #+ecl (declare (ignore stream))
+    #+ccl
+    (ccl:stream-device stream :output)
+    #+clisp
+    (multiple-value-bind (input-handle output-handle)
+	(handler-case (ext:stream-handles stream)
+	  (system::simple-stream-error ()
+	    nil))
+      (declare (ignore input-handle))
+      output-handle)
+    #+allegro
+    (let ((handle (excl::stream-output-handle stream)))
+      (when (numberp handle)
+	handle))
+    #-(or ccl clisp allegro)
     nil))
 
 #+ecl
@@ -113,86 +147,64 @@ Return two values:
 
 ;; #### NOTE: ABCL doesn't appear below because this module (termio) is never
 ;; loaded with it.
-(defun stream-line-width (stream)
+(defun stream-line-width
+    (stream &aux (handle (stream-ioctl-output-handle stream)))
   "Get STREAM's line width.
 Return two values:
 - the stream's line width, or nil if it can't be computed
   (typically when the stream does not denote a tty),
 - an error message if the operation failed."
-  ;; #### NOTE: doing a TIOCGWINSZ ioctl here is a convenient way to both know
-  ;; whether we're connected to a tty, and getting the terminal width at the
-  ;; same time. In case the ioctl fails, we need to distinguish between and
-  ;; ENOTTY error, which simply means that we're not connected to a terminal,
-  ;; and the other which are real errors and need to be reported.
-  ;; #### PORTME.
-  #+sbcl
-  (locally (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
-    (let ((file-stream (stream-file-stream stream :output)))
-      (when file-stream
-	(handler-case
-	    (with-winsize winsize ()
-	      (sb-posix:ioctl file-stream +tiocgwinsz+ winsize)
-	      (winsize-ws-col winsize))
-	  (sb-posix:syscall-error (error)
-	    (unless (= (sb-posix:syscall-errno error) sb-posix:enotty)
-	      (values nil error)))))))
-  #+cmu
-  (locally (declare (optimize (ext:inhibit-warnings 3)))
-    (let ((file-stream (stream-file-stream stream :output)))
-      (when file-stream
-	(alien:with-alien ((winsize (alien:struct unix:winsize)))
-	  (multiple-value-bind (success error-number)
-	      (unix:unix-ioctl
-	       (system:fd-stream-fd file-stream)
-	       unix:tiocgwinsz
-	       winsize)
-	    (if success
-		(alien:slot winsize 'unix:ws-col)
-	      (unless (= error-number unix:enotty)
-		(values nil (unix:get-unix-error-msg error-number)))))))))
-  #+ccl
-  (let ((fd (ccl::stream-device stream :output)))
-    (when fd
-      (ccl:rlet ((winsize :winsize))
-	(let ((result (ccl::int-errno-call
-		       (#_ioctl fd #$TIOCGWINSZ :address winsize))))
-	  (if (zerop result)
-	      (ccl:pref winsize :winsize.ws_col)
-	    (unless (= result (- #$ENOTTY))
-	      (values nil (ccl::%strerror (- result)))))))))
-  #+ecl
-  (when (stream-file-stream stream :output)
+  (when handle
+    ;; #### NOTE: doing a TIOCGWINSZ ioctl here is a convenient way to both
+    ;; know whether we're connected to a tty, and getting the terminal width
+    ;; at the same time. In case the ioctl fails, we need to distinguish
+    ;; between and ENOTTY error, which simply means that we're not connected
+    ;; to a terminal, and the other which are real errors and need to be
+    ;; reported.
+    ;; #### PORTME.
+    #+sbcl
+    (locally (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+      (handler-case
+	  (with-winsize winsize ()
+	    (sb-posix:ioctl handle +tiocgwinsz+ winsize)
+	    (winsize-ws-col winsize))
+	(sb-posix:syscall-error (error)
+	  (unless (= (sb-posix:syscall-errno error) sb-posix:enotty)
+	    (values nil error)))))
+    #+cmu
+    (locally (declare (optimize (ext:inhibit-warnings 3)))
+      (alien:with-alien ((winsize (alien:struct unix:winsize)))
+	(multiple-value-bind (success error-number)
+	    (unix:unix-ioctl handle unix:tiocgwinsz winsize)
+	  (if success
+	      (alien:slot winsize 'unix:ws-col)
+	    (unless (= error-number unix:enotty)
+	      (values nil (unix:get-unix-error-msg error-number)))))))
+    #+ccl
+    (ccl:rlet ((winsize :winsize))
+      (let ((result (ccl::int-errno-call
+		     (#_ioctl handle #$TIOCGWINSZ :address winsize))))
+	(if (zerop result)
+	    (ccl:pref winsize :winsize.ws_col)
+	  (unless (= result (- #$ENOTTY))
+	    (values nil (ccl::%strerror (- result)))))))
+    #+ecl
     (multiple-value-bind (cols msg)
-	(fd-line-width (ext:file-stream-fd stream))
-      (values (unless (= cols -1) cols) msg)))
-  #+(or clisp allegro lispworks)
-  (let ((fd #+clisp     (multiple-value-bind (input-fd output-fd)
-			    (ext:stream-handles stream)
-			  (declare (ignore input-fd))
-			  output-fd)
-	    #+allegro   (excl::stream-output-handle stream)
-	    #+lispworks (let ((file-stream
-				(stream-file-stream stream :output)))
-			  (cond (file-stream
-				 (stream::os-file-handle-stream-file-handle
-				  file-stream))
-				((stream-terminal-stream stream :output)
-				 1)
-				(t
-				 nil)))))
-    (when fd
-      (cffi:with-foreign-object (winsize 'winsize)
-	(let ((result (cffi:foreign-funcall "ioctl"
-			:int fd
-			:int +tiocgwinsz+
-			:pointer winsize
-			:int)))
-	  (if (= result -1)
-	      (unless (= +errno+ +enotty+)
-		(values nil
-			(cffi:foreign-funcall "strerror"
-			  :int +errno+ :string)))
-	    (cffi:with-foreign-slots ((ws-col) winsize winsize)
-	      ws-col)))))))
+	(fd-line-width handle)
+      (values (unless (= cols -1) cols) msg))
+    #+(or clisp allegro lispworks)
+    (cffi:with-foreign-object (winsize 'winsize)
+      (let ((result (cffi:foreign-funcall "ioctl"
+		      :int handle
+		      :int +tiocgwinsz+
+		      :pointer winsize
+		      :int)))
+	(if (= result -1)
+	    (unless (= +errno+ +enotty+)
+	      (values nil
+		      (cffi:foreign-funcall "strerror"
+			:int +errno+ :string)))
+	  (cffi:with-foreign-slots ((ws-col) winsize winsize)
+	    ws-col))))))
 
 ;;; termio.lisp ends here
